81a75d32725916afbc6bfb963c1ac44c0cffd18f
yaronday <yaronday77@gmail.com>
2025-10-29 03:40:26 +0200

    Addint imports file_tree_display

--- src/nano_dev_utils/__init__.py
+++ src/nano_dev_utils/__init__.py
@@ -1,16 +1,19 @@
 """nano-dev-utils - A collection of small Python utilities for developers.
 Copyright (c) 2025 Yaron Dayan
 """
-
+from pathlib import Path
 from importlib.metadata import version
 from .dynamic_importer import Importer
 from .timers import Timer
 from .release_ports import PortsRelease, PROXY_SERVER, INSPECTOR_CLIENT
 from .common import update
+from .file_tree_display import FileTreeDisplay
 
 timer = Timer()
 ports_release = PortsRelease()
 importer = Importer()
+filetree_display = FileTreeDisplay(root_dir=str(Path.cwd()))
+
 
 __version__ = version('nano-dev-utils')
 
@@ -24,4 +27,5 @@ __all__ = [
     'timer',
     'ports_release',
     'importer',
+    'filetree_display'
 ]

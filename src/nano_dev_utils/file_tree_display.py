9996a3404905c2c2a43bf256f164460dae847f8b
yaronday <yaronday77@gmail.com>
2025-11-02 00:58:10 +0200

    Refactor FileTreeDisplay with predicate builder; Addional functionality: inclusion of files and folders Improving the performance of build_tree utilizing the predicate funcs for filtering files and folders.

--- src/nano_dev_utils/file_tree_display.py
+++ src/nano_dev_utils/file_tree_display.py
@@ -1,12 +1,11 @@
 import os
 import re
-import fnmatch
 
 from collections.abc import Generator
 from pathlib import Path
 from typing_extensions import LiteralString, Callable, Any
 
-from .common import str2file
+from .common import str2file, FilterSet, PredicateBuilder
 
 
 DEFAULT_SFX = '_filetree.txt'
@@ -23,15 +22,17 @@ class FileTreeDisplay:
     visual representations of directories and files.
     Supports exclusion lists, configurable indentation, and custom prefix styles.
     """
-
     def __init__(
             self,
             root_dir: str | None = None,
             filepath: str | None = None,
-            ignore_dirs: list[str] | set[str] | None = None,
-            ignore_files: list[str] | set[str] | None = None,
+            ignore_dirs: FilterSet = None,
+            ignore_files: FilterSet = None,
+            include_dirs: FilterSet = None,
+            include_files: FilterSet = None,
             style: str = ' ',
-            indent: int = 1,
+            indent: int = 2,
+            files_first: bool = False,
             sort_key_name: str = 'natural',
             reverse: bool = False,
             custom_sort: Callable[[str], Any] | None = None,
@@ -45,20 +46,27 @@ class FileTreeDisplay:
             filepath: str | None: full output file path.
             ignore_dirs (list[str] | set[str] | None): Directory names or patterns to ignore.
             ignore_files (list[str] | set[str] | None): File names or patterns to ignore.
-            style (str): Character(s) used to represent hierarchy levels. Defaults to "-".
+            include_dirs (list[str] | set[str] | None): Directory names or patterns to include.
+            include_files (list[str] | set[str] | None): File names or patterns to include.
+            style (str): Character(s) used to represent hierarchy levels. Defaults to " ".
             indent (int): Number of style characters used per hierarchy level. Defaults to 2.
-            sort_key_name (str): sorting key name, e.g. 'lex' for lexicographic or 'custom'. Default to 'natural'.
-            reverse (bool): reversed sorting
+            files_first (bool): Determines whether to list files first. Defaults to False.
+            sort_key_name (str): sorting key name, e.g. 'lex' for lexicographic or 'custom'. Defaults to 'natural'.
+                                 '' means no sorting.
+            reverse (bool): reversed sorting.
             custom_sort (Callable[[str], Any] | None):
             save2file (bool): save file tree info to a file.
-            printout (bool): print file tree info
+            printout (bool): print file tree info.
         """
         self.root_path = Path(root_dir) if root_dir else Path.cwd()
         self.filepath = filepath
         self.ignore_dirs = set(ignore_dirs or [])
         self.ignore_files = set(ignore_files or [])
+        self.include_dirs = set(include_dirs or [])
+        self.include_files = set(include_files or [])
         self.style = style
         self.indent = indent
+        self.files_first = files_first
         self.sort_key_name = sort_key_name
         self.reverse = reverse
         self.custom_sort = custom_sort
@@ -68,14 +76,26 @@ class FileTreeDisplay:
         self.sort_keys = {
             'natural': self._nat_key,
             'lex': self._lex_key,
-            'custom': None,
+            'custom': self.custom_sort,
+            '': None,
         }
+
+        self.pb = PredicateBuilder()
+        self.dir_filter = self.pb.build_predicate(self.include_dirs, self.ignore_dirs)
+        self.file_filter = self.pb.build_predicate(self.include_files, self.ignore_files)
 
     def init(self, *args, **kwargs) -> None:
         self.__init__(*args, **kwargs)
 
     def update(self, attrs: dict) -> None:
         self.__dict__.update(attrs)
+        pattern = re.compile(r'^(ign|inc)')
+        if any(pattern.match(key) for key in attrs):
+            self.update_predicates()
+
+    def update_predicates(self):
+        self.dir_filter = self.pb.build_predicate(self.include_dirs, self.ignore_dirs)
+        self.file_filter = self.pb.build_predicate(self.include_files, self.ignore_files)
 
     @staticmethod
     def _nat_key(name: str) -> list[int | LiteralString]:
@@ -121,8 +141,9 @@ class FileTreeDisplay:
         lines.extend(list(iterator))
         return '\n'.join(lines)
 
-    def build_tree(self, dir_path: str, prefix: str = '') -> Generator[str, None, None]:
-        """Recursively yield formatted directory tree lines, using DFS.
+    def build_tree(self, dir_path: str,
+                   prefix: str = '') -> Generator[str, None, None]:
+        """Yields formatted directory tree lines, using a recursive DFS.
         Intended order of appearance is with a preference to subdirectories first.
 
         Args:
@@ -132,69 +153,53 @@ class FileTreeDisplay:
         Yields:
             str: A formatted string representing either a directory or a file.
         """
-        style, indent, should_ignore = self.style, self.indent, self.should_ignore
-        join_prefix = ''.join
+        files_first = self.files_first
+        dir_filter, file_filter = self.dir_filter, self.file_filter
+        sort_key_name, reverse = self.sort_key_name, self.reverse
+        sort_key = self.sort_keys.get(self.sort_key_name)
+        curr_indent = self.style * self.indent
 
-        sort_key: Callable[[str], Any] | None = None
-        reverse = self.reverse
-        sort_key_name = self.sort_key_name
-        if sort_key_name:
+        next_prefix = prefix + curr_indent
+
+        if sort_key is None:
             if sort_key_name == 'custom':
-                if not self.custom_sort:
-                    raise ValueError("custom_sort function must "
-                                     "be specified when sort_key_name='custom'")
-                sort_key = self.custom_sort
-            else:
-                sort_key = self.sort_keys.get(sort_key_name)
-                if sort_key is None:
-                    raise ValueError(f"Invalid sort key name: {sort_key_name}")
-
-        curr_indent: str = style * indent
-        next_prefix = f'{prefix}{curr_indent}'
+                raise ValueError("custom_sort function must be specified"
+                                 " when sort_key_name='custom'")
+            raise ValueError(f"Invalid sort key name: {sort_key_name}")
 
         try:
             with os.scandir(dir_path) as entries:
                 dirs, files = [], []
                 append_dir, append_file = dirs.append, files.append
-
                 for entry in entries:
                     name = entry.name
                     if entry.is_dir():
-                        if not should_ignore(name, True):
+                        if dir_filter(name):
                             append_dir((name, entry.path))
                     else:
-                        if not should_ignore(name, False):
+                        if file_filter(name):
                             append_file(name)
+
         except (PermissionError, OSError) as e:
             msg = '[Permission Denied]' if isinstance(e, PermissionError) else '[Error reading directory]'
-            yield f'{prefix}{curr_indent}{msg}'
+            yield f'{next_prefix}{msg}'
             return
 
         if sort_key:
             dirs.sort(key=lambda d: sort_key(d[0]), reverse=reverse)
             files.sort(key=sort_key, reverse=reverse)
 
+        if files_first:
+            for name in files:
+                yield next_prefix + name
+
         for name, path in dirs:
-            yield join_prefix((prefix, curr_indent, name, '/'))
+            yield f'{next_prefix}{name}/'
             yield from self.build_tree(path, next_prefix)
 
-        for name in files:
-            yield join_prefix((next_prefix, name))
-
-    def should_ignore(self, name: str, is_dir: bool) -> bool:
-        """Return True if a name matches ignore patterns.
-
-        Args:
-            name (str): The file or directory name to check.
-            is_dir (bool): Whether the name refers to a directory.
-
-        Returns:
-            bool: True if the name matches any ignore pattern, else False.
-        """
-        ignore_set = self.ignore_dirs if is_dir else self.ignore_files
-        if name in ignore_set:
-            return True
-        return any(fnmatch.fnmatch(name, pattern) for pattern in ignore_set)
+        if not files_first:
+            for name in files:
+                yield next_prefix + name
 
     def format_out_path(self) -> Path:
         alt_file_name = f'{self.root_path.name}{DEFAULT_SFX}'
